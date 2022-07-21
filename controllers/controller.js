dd5982a7af8d428739c4c533b5c7a26485f86b18
sugiartomi <sugiartomi@gmail.com>
2022-07-21 11:20:37 +0700

    login-owner

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,23 @@
-let { User, Category, Item } = require('../models')
+let { User, Category, Product, Profile } = require('../models')
 
 class Controller {
+ static loginOwner(req, res) {
+    res.render('loginOwner')
+ }
 
+ static loginOwnerPost(req,res) {
+    let {email, password} = req.body
+    User.findAll()
+    .then(data => {
+        data.forEach(el => {
+            if(el.email == email && el.password == password){
+                res.render('owner')
+            } else throw "invalid email or password"
+        });
+
+    })
+    .catch(err => res.send(err))
+ }
 }
 
 module.exports = Controller
--- router/index.js
+++ router/index.js
@@ -1,27 +1,31 @@
 const router = require('express').Router()
 const Controller = require('../controllers/controller')
 
-router.get('/', Controller.xxx)
-router.post('/', Controller.xxx)
+router.get('/login-owner', Controller.loginOwner)
+router.post('/login-owner', Controller.loginOwnerPost)
 
-router.get('/owner', Controller.xxx)
-router.get('/owner/addUser', Controller.xxx)
-router.post('/owner/addUser', Controller.xxx)
-router.get('/owner/editUser', Controller.xxx)
-router.post('/owner/editUser', Controller.xxx)
-router.get('/owner/deleteUser', Controller.xxx)
 
-router.get('/cashier/:id', Controller.xxx)
-router.get('/cashier/:cahierId/detail/:ProductId', Controller.xxx)
-router.get('/cashier/:cahierId/buy/:ProductId', Controller.xxx)
-router.post('/cashier/:cahierId/buy/:ProductId', Controller.xxx)
+// router.post('/', Controller.xxx)
+// router.get('/owner', Controller.xxx)
+// router.get('/owner/addUser', Controller.xxx)
+// router.post('/owner/addUser', Controller.xxx)
+// router.get('/owner/editUser', Controller.xxx)
+// router.post('/owner/editUser', Controller.xxx)
+// router.get('/owner/deleteUser', Controller.xxx)
 
-router.get('/warehouse/:id', Controller.xxx)
-router.get('/warehouse/:id/add', Controller.xxx)
-router.post('/warehouse/:id/add', Controller.xxx)
-router.get('/warehouse/:id/edit', Controller.xxx)
-router.post('/warehouse/:id/edit', Controller.xxx)
-router.get('/warehouse/:id/delete', Controller.xxx)
+// //MWowner
+
+// router.get('/cashier/:id', Controller.xxx)
+// router.get('/cashier/:cahierId/detail/:ProductId', Controller.xxx)
+// router.get('/cashier/:cahierId/buy/:ProductId', Controller.xxx)
+// router.post('/cashier/:cahierId/buy/:ProductId', Controller.xxx)
+
+// router.get('/warehouse/:id', Controller.xxx)
+// router.get('/warehouse/:id/add', Controller.xxx)
+// router.post('/warehouse/:id/add', Controller.xxx)
+// router.get('/warehouse/:id/edit', Controller.xxx)
+// router.post('/warehouse/:id/edit', Controller.xxx)
+// router.get('/warehouse/:id/delete', Controller.xxx)
 
 
 module.exports = router
